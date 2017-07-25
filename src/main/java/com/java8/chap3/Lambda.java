dd165603769c7ef1dcfb7382d04bb961f98a8764
pengttyy <pengttyy@gmail.com>
2017-07-25 22:05:19 +0800

    feat(lambda函数描述符):方法签名的lambda版本
    使用(T)-R的形式来描述函数式接口，小括号中代码参数，-后面代码返回值类型

--- src/main/java/com/java8/chap3/Lambda.java
+++ src/main/java/com/java8/chap3/Lambda.java
@@ -9,17 +9,23 @@ import java.util.function.Function;
  */
 public class Lambda {
 
-    public void defindLambda(){
+    public void defindLambda() {
         Consumer<String> stringConsumer = (String s) -> System.out.println(s);
         Runnable tt = () -> System.out.println("tt");
-        Function<String,Integer> func = (String s) -> s.length();
+        Function<String, Integer> func = (String s) -> s.length();
         DoubleSupplier doubleSupplier = () -> 50;
-        Runnable runnable = () -> {};
+        Runnable runnable = () -> {
+        };
         //()-> return "test";
         DoubleSupplier doubleSupplier1 = () -> {
             return 10;
         };
-        Runnable runnable1 = () -> {};
+        Runnable runnable1 = () -> {
+        };
+
+        //如Runnable函数式接口的函数描述符可以表示为“()-void”
+        //Function的函数式接口的描述符可以表示为“(T)-R”
+
     }
 
 }
