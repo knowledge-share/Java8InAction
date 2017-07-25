82ea2228d3ea0f7962c8830c95982ab61559ca98
pengttyy <pengttyy@gmail.com>
2017-07-25 20:59:01 +0800

    feat(lambda定义):有参数有返回值定义

--- src/main/java/com/java8/chap3/Lambda.java
+++ src/main/java/com/java8/chap3/Lambda.java
@@ -1,6 +1,7 @@
 package com.java8.chap3;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Created by pengt on 2017/7/25.
@@ -10,6 +11,7 @@ public class Lambda {
     public void defindLambda(){
         Consumer<String> stringConsumer = (String s) -> System.out.println(s);
         Runnable tt = () -> System.out.println("tt");
+        Function<String,Integer> func = (String s) -> s.length();
     }
 
 }
